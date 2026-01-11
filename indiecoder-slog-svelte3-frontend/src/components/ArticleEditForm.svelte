a2e95d9fdd863d87946c48e89629f70f1c4b0118
유재혁 <yjk7454@naver.com>
2026-01-12 00:17:23 +0900

    feat: ArticlesEditForm.svelte - 게시글 수정 구현: 수정모드 open/close store 컴포넌트 적용

--- indiecoder-slog-svelte3-frontend/src/components/ArticleEditForm.svelte
+++ indiecoder-slog-svelte3-frontend/src/components/ArticleEditForm.svelte
@@ -1,21 +1,36 @@
+<script>
+  export let article
+  import { articles, auth } from '../stores'
+  let articleValue = {
+    id: article.id,
+    userEmail: article.userEmail,
+    createdAt: article.createdAt,
+    content: article.content,
+  }
+  const closeEditModeArticle = () => {
+    articles.closeEditModeArticle()
+  }
+</script>
+
+
 <!-- articles.html -->
 <!-- slog-content-edit-form start-->
 <div class="slog-content-box" >
   <div class="content-box-header">
     <div class="content-box-header-inner-left" >
-      <p class="p-user" >freeseamew</p>
-      <p class="p-date" >2022-11-11</p>
+      <p class="p-user" >{articleValue.userEmail}</p>
+      <p class="p-date" >{articleValue.createdAt}</p>
     </div>
   </div>
   
   <div class="content-box-main">
-    <textarea id="message" rows="5" class="slog-content-textarea " placeholder="내용을 입력해 주세요."></textarea>
+    <textarea bind:value={articleValue.content} id="message" rows="5" class="slog-content-textarea " placeholder="내용을 입력해 주세요."></textarea>
   </div>
   
   <div class="content-box-bottom">
     <div class="button-box">
       <button class="button-base">완료</button>
-      <button class="button-base">취소</button>
+      <button class="button-base" on:click={closeEditModeArticle}>취소</button>
     </div>
   </div>
 </div>
