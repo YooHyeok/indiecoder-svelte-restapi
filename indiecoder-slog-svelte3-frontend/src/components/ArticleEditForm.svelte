9afda5325e67c943b3c748f10df38f1701475054
유재혁 <yjk7454@naver.com>
2026-01-13 08:15:10 +0900

    feat: `ArticleEditForm.svelte` - 게시글 수정 구현: 수정 api 호출 store 컴포넌트 적용

--- indiecoder-slog-svelte3-frontend/src/components/ArticleEditForm.svelte
+++ indiecoder-slog-svelte3-frontend/src/components/ArticleEditForm.svelte
@@ -7,8 +7,11 @@
     createdAt: article.createdAt,
     content: article.content,
   }
-  const closeEditModeArticle = () => {
+  const onCoseEditModeArticle = () => {
     articles.closeEditModeArticle()
+  }
+  const onUpdateArticle = () => {
+    articles.updateArticle(articleValue)
   }
 </script>
 
@@ -28,8 +31,8 @@
   
   <div class="content-box-bottom">
     <div class="button-box">
-      <button class="button-base">완료</button>
-      <button class="button-base" on:click={closeEditModeArticle}>취소</button>
+      <button class="button-base" on:click={onUpdateArticle}>완료</button>
+      <button class="button-base" on:click={onCoseEditModeArticle}>취소</button>
     </div>
   </div>
 </div>
