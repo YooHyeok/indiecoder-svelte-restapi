9fb58f819684bd8ac29d45db1fdc28c4bf55692b
유재혁 <yjk7454@naver.com>
2026-01-27 08:35:07 +0900

    feat: `utils/date.js` - 날짜보기:  작성일로 부터 몇일 전인지에 대한 relativeTime 모듈 구현

---
+++ indiecoder-slog-svelte3-frontend/src/utils/date.js
@@ -0,0 +1,13 @@
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime"
+import utc from "dayjs/plugin/utc"
+import "dayjs/locale/ko"
+
+function dateView(date) {
+  dayjs.extend(utc)
+  dayjs.locale('ko')
+  dayjs.extend(relativeTime)
+
+  return dayjs().to(dayjs(date).utc().format('YYYY-MM-DD HH:mm:ss'))
+}
+export default dateView
