4756b71e4134df6aacac517897d696dcb76534fa
유재혁 <yjk7454@naver.com>
2026-01-28 00:30:12 +0900

    feat: `router.svelte` -  URL 기반 보기모드:  라우터 구성 변경

--- indiecoder-slog-svelte3-frontend/src/router.svelte
+++ indiecoder-slog-svelte3-frontend/src/router.svelte
@@ -4,9 +4,16 @@
   import Login from './pages/Login.svelte';
   import Register from './pages/Register.svelte';
   import NotFound from './pages/notFound.svelte';
+  import { isLogin } from './stores';
 </script>
 <Route path="/" redirect="/articles/all" />
-<Route path="/articles/*" ><Articles/></Route>
-<Route path="/login" ><Login/></Route>
-<Route path="/register" ><Register/></Route>
-<Route fallback ><NotFound/></Route>
+<Route path="/articles/*">
+  <Route path="/all/*"><Articles/></Route>
+  {#if $isLogin}
+    <Route path="/my/*"><Articles/></Route>
+    <Route path="/like/*"><Articles/></Route>
+  {:else}
+    <Route path="/my/*" redirect="/articles/all"/>
+    <Route path="/like/*" redirect="/articles/all"><Articles/></Route>
+  {/if}
+</Route>
