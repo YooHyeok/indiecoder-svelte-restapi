503b5e4e587bc860a4b614090306d71a37799ca7
유재혁 <yjk7454@naver.com>
2026-01-30 08:32:01 +0900

    feat: `ArticleList.svelte` - 무한스크롤 컴포넌트화: InfiniteScroll컴포넌트 적용 및 관련 로직 제거

--- indiecoder-slog-svelte3-frontend/src/components/ArticleList.svelte
+++ indiecoder-slog-svelte3-frontend/src/components/ArticleList.svelte
@@ -1,16 +1,14 @@
 <script>
   import Article from "./Article.svelte";
-  import ArticleLoading from "./ArticleLoading.svelte";
   import { onMount } from 'svelte'
   import { 
     articles, currentArticlesPage,
     loadingArticle, articlePageLock, articlesMode
    } from '../stores'
   import { router } from 'tinro'
+  import InfiniteScroll from "./InfiniteScroll.svelte";
   
   /* 스크롤 정보를 담을 상태값 */
-  let component
-  let element
   let currentMode = $router.path.split("/")[2]
 
   onMount(() => {
@@ -18,46 +16,11 @@
     // articles.fetchArticles()
     articlesMode.changeMode(currentMode)
   })
-  
-  $: {
-    if (component) {
-      element = component.parentNode
-      element.addEventListener('scroll', onScroll)
-      element.addEventListener('resize', onScroll)
-    }
-  }
-
-  const onScroll = (e) => {
-    const scrollHeight = e.target.scrollHeight // 스크롤 높이
-    const clientHeight = e.target.clientHeight // 화면 높이
-    const scrollTop = e.target.scrollTop // 현재 스크롤 위치
-    const realHeight = scrollHeight - clientHeight // 실제 스크롤 높이
-    const triggerHeight = realHeight * 0.7 // 화면 70%에 해당하는 높이(다음 페이지가 호출될 스크롤 위치)
-
-    const triggerComputed = () => {
-      return scrollTop > triggerHeight
-    }
-    const countCheck = () => {
-      const check = $articles.totalPageCount <= $currentArticlesPage 
-      return check
-    }
-
-    if(countCheck()) {// 전체 페이지보다 현재 호출된 페이지보다 작거나 같은 경우 조회 잠금
-      articlePageLock.set(true)
-    }
-
-    const scrollTrigger = () => {
-      return triggerComputed() && !countCheck() && !$articlePageLock
-    }
-    if (scrollTrigger()) {
-      currentArticlesPage.increPage()
-    }
-  }
 
 </script>
 <!-- articles.html -->
 <!-- slog-list-wrap start-->
-<div class="slog-list-wrap" bind:this={component}>    
+<div class="slog-list-wrap infiniteTarget">    
   <ul class="slog-ul">
     {#each $articles.articleList as article, index}
       <li class="mb-5">
@@ -65,7 +28,13 @@
       </li>
     {/each}
   </ul>
-  {#if $loadingArticle}
-    <ArticleLoading />
-  {/if}
+  <InfiniteScroll 
+    loading={$loadingArticle}
+    pageLock={$articlePageLock}
+    totalPageCount={$articles.totalPageCount}
+    currentPage={$currentArticlesPage}
+    domTarget={'.infiniteTarget'}
+    on:onPageLock={() => articlePageLock.set(true)}
+    on:increPage={() => currentArticlesPage.increPage()}
+  />
 </div><!-- slog-list-wrap end-->
